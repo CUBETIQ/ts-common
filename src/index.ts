c42964407eaa35f0beea28926d234f74678b6cfc
Sambo Chea <sombochea@cubetiqs.com>
2021-09-13 20:16:35 +0700

    Fixed and updated the tsconfig and package for ts-common

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
-import './date'
-import './log'
-import './log/writter'
-import './number/parser'
+// import { formatDate, formatDateOnly, formatDateFromTimestamp } from "./date"
+// import { debug, info,error, warn } from './log'
+// import { parse2Int, parse2IntOrNull, parseToBoolean } from "./number/parser"
+
+// export {
+//     formatDate,
+//     formatDateOnly,
+//     formatDateFromTimestamp,
+//     debug,
+//     info,
+//     error,
+//     warn,
+//     parse2Int,
+//     parse2IntOrNull,
+//     parseToBoolean,
+// }
